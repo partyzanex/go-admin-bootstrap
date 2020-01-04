2ac7c5c35ef10e39fffc49652be711c973d23daf
Dmitry Zolotarev <partyzan65@gmail.com>
2020-01-04 18:01:29 +0300

    breadcrumbs sort

--- widgets/breadrumbs.go
+++ widgets/breadrumbs.go
@@ -1,23 +1,37 @@
 package widgets
 
+import "sort"
+
 type breadcrumb struct {
-	Name   string
-	URL    string
-	Active bool
+	Name      string
+	URL       string
+	Active    bool
+	SortOrder int
 }
 
 type Breadcrumbs []breadcrumb
 
-func (b *Breadcrumbs) Add(name, url string) {
+func (b *Breadcrumbs) Add(name, url string, sortOrder int) {
 	items := *b
 	for i := range items {
 		items[i].Active = false
 	}
 
 	items = append(items, breadcrumb{
-		Name:   name,
-		URL:    url,
-		Active: true,
+		Name:      name,
+		URL:       url,
+		Active:    true,
+		SortOrder: sortOrder,
+	})
+
+	*b = items
+}
+
+func (b *Breadcrumbs) Sort() {
+	items := *b
+
+	sort.Slice(items, func(i, j int) bool {
+		return items[i].SortOrder < items[j].SortOrder
 	})
 
 	*b = items
